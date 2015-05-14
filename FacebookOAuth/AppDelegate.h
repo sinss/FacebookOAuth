3445899f2d83ea324130320bc6b07c4813e3d72b
里歐張 <leo.chang@kland.com.tw>
2015-05-14 10:13:33 +0800

    Initial Commit

---
+++ FacebookOAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookOAuth
+//
+//  Created by Leo Chang on 5/14/15.
+//  Copyright (c) 2015 SpringHouse Entertainment Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookOAuth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacebookOAuth
+//
+//  Created by Leo Chang on 5/14/15.
+//  Copyright (c) 2015 SpringHouse Entertainment Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
